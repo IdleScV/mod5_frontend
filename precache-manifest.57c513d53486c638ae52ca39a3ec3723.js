829324caa8d109d39a86a6e232f86b0e6da253ef
Wesley Chen <weschen.github@gmail.com>
2020-05-16 16:42:53 -0400

    Updates

---
+++ precache-manifest.57c513d53486c638ae52ca39a3ec3723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0c4a0f59e5a3b11d20ea48e4f1d1df",
+    "url": "/mod5_frontend/index.html"
+  },
+  {
+    "revision": "3eaa61c9e6085afe9a99",
+    "url": "/mod5_frontend/static/css/main.7ce80d26.chunk.css"
+  },
+  {
+    "revision": "d35205fac55d5f8aea41",
+    "url": "/mod5_frontend/static/js/2.bc31d966.chunk.js"
+  },
+  {
+    "revision": "265068e7f3bc904803e029152d67e9a1",
+    "url": "/mod5_frontend/static/js/2.bc31d966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eaa61c9e6085afe9a99",
+    "url": "/mod5_frontend/static/js/main.49506f35.chunk.js"
+  },
+  {
+    "revision": "10b6e5c419b9baeca45b",
+    "url": "/mod5_frontend/static/js/runtime-main.e7c0eece.js"
+  }
+]);
