bb9eb58f1f8bdd0e51e42842e5a3111f8df9066c
Wesley Chen <weschen.github@gmail.com>
2020-05-18 15:04:40 -0400

    update routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,5 @@
-export const SIGN_UP = '/mod5_frontend/signup';
-export const SIGN_IN = '/mod5_frontend/signin';
-export const ACCOUNT = '/mod5_frontend/account';
-export const PASSWORD_FORGET = '/mod5_frontend/pw-forget';
-export const HOME = '/mod5_frontend/';
+export const SIGN_UP = '/Path_frontend/signup';
+export const SIGN_IN = '/Path_frontend/signin';
+export const ACCOUNT = '/Path_frontend/account';
+export const PASSWORD_FORGET = '/Path_frontend/pw-forget';
+export const HOME = '/Path_frontend/';
--- src/urlEnv/index.js
+++ src/urlEnv/index.js
@@ -2,10 +2,10 @@
 export const URL = 'https://immense-savannah-15383.herokuapp.com/';
 
 export const ROUTES = {
-	HOME: '/mod5_frontend/',
-	TIMELINES: '/mod5_frontend/timelines',
-	TIMELINE: '/mod5_frontend/timeline/:id',
-	PROFILE: '/mod5_frontend/profile',
-	CREATE: '/mod5_frontend/create',
-	TIMELINEGO: '/mod5_frontend/timeline/'
+	HOME: '/Path_frontend/',
+	TIMELINES: '/Path_frontend/timelines',
+	TIMELINE: '/Path_frontend/timeline/:id',
+	PROFILE: '/Path_frontend/profile',
+	CREATE: '/Path_frontend/create',
+	TIMELINEGO: '/Path_frontend/timeline/'
 };
